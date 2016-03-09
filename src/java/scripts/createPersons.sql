67863caebbda12a27ff08380fcea6bc453cd7bb2
sofus <sofusalbertsen@gmail.com>
2016-03-09 11:11:27 +0100

    Ready for todays examples

--- src/java/scripts/createPersons.sql
+++ src/java/scripts/createPersons.sql
@@ -1,6 +1,6 @@
-delete from Person;
+delete from PERSON;
 
-insert into Person values (null,'Peter','Olsen','1234');
-insert into Person values (null,'Hanne','Olsen','1234');
-insert into Person values (null,'Kurt','Wonnegut','7985476');
-insert into Person values (null,'Heidi','Petersen','1234');
+insert into PERSON values (null,'Peter','Olsen','1234');
+insert into PERSON values (null,'Hanne','Olsen','1234');
+insert into PERSON values (null,'Kurt','Wonnegut','7985476');
+insert into PERSON values (null,'Heidi','Petersen','1234');
--- src/java/test/Tester.java
+++ src/java/test/Tester.java
@@ -8,7 +8,7 @@ import javax.persistence.Persistence;
 public class Tester {
   
   public static void main(String[] args) {
-    Persistence.generateSchema("pu_production", null);
+    Persistence.generateSchema("pu_development", null);
     Persistence.generateSchema("pu_test", null);
   }
   
